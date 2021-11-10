b03e4d35e75f3ce62193d107587894d0c44a1ee8
AndrewStaus <94034810+AndrewStaus@users.noreply.github.com>
2021-11-10 01:51:47 -0700

    Delete replay.py

--- replay.py
+++
@@ -1,9 +0,0 @@
-from training import worker
-import pickle
-
-if __name__ == '__main__':
-
-    with open(r'.\Trained Agents\1410 trained_agent_16_Relu.pickle', 'rb') as f:
-        agent = pickle.load(f)
-
-    worker(agent, graphics=True)
