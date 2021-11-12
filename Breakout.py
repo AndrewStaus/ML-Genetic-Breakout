65a25cac7293935060f4a038c742bbecf3289d52
Andrew Staus <94034810+AndrewStaus@users.noreply.github.com>
2021-11-11 23:44:40 -0700

    Update Breakout.py

--- Breakout.py
+++ Breakout.py
@@ -1,17 +1,11 @@
-'''
-<h1>Breakout.py</h1>
-<p>
+'''#Breakout.py
 Launch the human playable version of breakout.
-<br>The agents make it look easy, give it a go for yourself!
-</p>
-<h3>Controls</h3>
-<p>
-    <ul>
-        <li><b>"W"</b> Move Left</li>
-        <li><b>"D"</b> -> Move Right</li>
-        <li><b>"Space"</b> -> Launch Projectile</li>
-    </ul>
-</p>
+The agents make it look easy, give it a go for yourself!
+
+### Controls
+    - "W": Move Left
+    - "D": Move Right
+    - "Space": Launch Projectile
 '''
 from lib import game_main
 if __name__ == "__main__":
